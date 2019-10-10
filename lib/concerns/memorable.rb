ce21e3c23d57f97f9483ced4078c38b4dff08d03
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-10 00:35:34 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,13 @@
 module Memorable
   module ClassMethods
   def reset_all
-     all.clear
+     self.all.clear
   end
   
   def count
-    all.count
+    self.all.count
   end
+end
   
   module InstanceMethods
     def initialize
