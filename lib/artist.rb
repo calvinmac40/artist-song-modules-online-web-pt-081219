f98abc3fc45b632035528f71cd12c1d7e08a6c4d
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-10 00:30:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,10 +10,7 @@ class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   
-  def initialize
-    # self.class.all << self
-    @songs = []
-  end
+  
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
